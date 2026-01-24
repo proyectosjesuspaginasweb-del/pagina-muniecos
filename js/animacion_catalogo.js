0992092062b0a361efdae3258e21584b432ffb8e
Jesus Cabrera <proyectosjesuspaginasweb@gmail.com>
2026-01-24 15:10:46 -0600

    ActualizacionPagina nuevos cambios con IA

--- js/animacion_catalogo.js
+++ js/animacion_catalogo.js
@@ -1,2 +1,11 @@
-
+$(function() {
+    $(window).scroll(function() {
+        var scroll  = $(window).scrollTop();
+        if (scroll >= 100) {
+            $('#NavXO').addClass('is-scrolled'); 
+        }else{
+            $('#NavXO').addClass('.is-scrolled')
+        }
+    });
+});
 
