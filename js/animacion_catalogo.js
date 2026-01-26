4f3ea9375d2c2277f260f8c9e472d72b4553f03b
Jesus Cabrera <proyectosjesuspaginasweb@gmail.com>
2026-01-26 07:53:26 -0600

    ActualizacionPagina Pagina en Css y JS

--- js/animacion_catalogo.js
+++ js/animacion_catalogo.js
@@ -2,10 +2,41 @@ $(function() {
     $(window).scroll(function() {
         var scroll  = $(window).scrollTop();
         if (scroll >= 100) {
-            $('#NavXO').addClass('is-scrolled'); 
+            $('.OptionMenu').addClass('.is-scrolled'); 
         }else{
-            $('#NavXO').addClass('.is-scrolled')
+            $('OptionMenu').addClass('.is-scrolled')
         }
     });
+});
+
+/*Efecto de size*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const steps = document.querySelectorAll(".scroll-step");
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach(entry => {
+
+                if (entry.isIntersecting) {
+                    // Quitamos active a todos
+                    steps.forEach(step => step.classList.remove("active"));
+
+                    // Activamos solo el que está visible
+                    entry.target.classList.add("active");
+
+                    handleScrollLock(entry.target);
+                }
+
+            });
+        },
+        {
+            threshold: 0.6
+        }
+    );
+
+    steps.forEach(step => observer.observe(step));
+
 });
 
